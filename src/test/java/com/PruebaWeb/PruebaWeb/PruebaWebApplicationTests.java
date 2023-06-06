3d0578edad925e4d43389f806a15188ab5e350a6
josegonzm <jgonzalez50464@ufide.ac.cr>
2023-06-05 21:31:05 -0600

    Practica #2

---
+++ src/test/java/com/PruebaWeb/PruebaWeb/PruebaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaWeb.PruebaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
